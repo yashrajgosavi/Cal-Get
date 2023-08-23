0a2a3a9904b6dec22c7d01f3f247c3c3302f3988
NuraYokai <yashrajgosavi2580@gmail.com>
2023-08-24 01:27:53 +0530

    Added styled-components! Created safe-area.component. Added :-     styled-components.

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeArea>
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </SafeArea>
+      <ExpoStatusBar style="autp" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
