96f5b42c52a7d60744fea38cbaebbcb3b5c20595
NuraYokai <yashrajgosavi2580@gmail.com>
2023-09-23 15:51:43 +0530

    feat: Updated

--- src/screens/account.styles.js
+++ src/screens/account.styles.js
@@ -1,4 +1,5 @@
-import { Dimensions } from "react-native";
+import { Button, Dimensions } from "react-native";
+import { Card, Divider, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 
 export const AccountScrollView = styled.ScrollView`
@@ -13,3 +14,30 @@ export const AccountBackground = styled.ImageBackground.attrs({
   align-items: center;
   justify-self: flex-start;
 `;
+
+export const AccountCard = styled(Card)`
+  background-color: wheat;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width}px;
+  max-width: 500px; // 500 is the maximum width
+  height: auto;
+  margin-top: ${(props) => props.marginTop}px;
+`;
+
+export const AccountDivider = styled(Divider)`
+  border: 1px;
+  margin: 10px;
+`;
+
+const windowWidth = Dimensions.get("window").width;
+
+export const AccountTextInput = styled(TextInput)`
+  background-color: transparent;
+  margin: 10px;
+  font-size: ${windowWidth < 500 ? "14px" : "16px"};
+  width: ${windowWidth < 500 ? 300 : 400}px;
+`;
+
+export const AccountButton = styled(Button)``;
--- src/screens/signup/register.screen.js
+++ src/screens/signup/register.screen.js
@@ -1,15 +1,71 @@
-import { Dimensions, Text } from "react-native";
-import React from "react";
-import { AccountBackground, AccountScrollView } from "../account.styles";
+import React, { useEffect, useState } from "react";
+import {
+  AccountBackground,
+  AccountButton,
+  AccountCard,
+  AccountDivider,
+  AccountScrollView,
+  AccountTextInput,
+} from "../account.styles";
+import { Button, Text, TextInput } from "react-native-paper";
+import { Dimensions, Platform } from "react-native";
 
-const RegisterScreen = () => {
+export default function RegisterScreen() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    Dimensions.get("window")
+  );
+
+  useEffect(() => {
+    function handleResize({ window }) {
+      setWindowDimensions(window);
+    }
+
+    const subscription = Dimensions.addEventListener("change", handleResize);
+    return () => subscription.remove();
+  }, []);
+
+  const cardWidth = Math.min(windowDimensions.width * 0.9, 500); // 500 is the maximum width
+  const marginTop =
+    Platform.OS === "web"
+      ? windowDimensions.height * 0.2
+      : windowDimensions.height * 0.15; // 20% of the window's height
+
+  const [email, setEmail] = React.useState("");
+
   return (
     <AccountScrollView>
       <AccountBackground>
-        <Text>RegisterScreen {Dimensions.get("screen").height} </Text>
+        <AccountCard width={cardWidth} marginTop={marginTop}>
+          <AccountCard.Content>
+            <Text variant="displaySmall">Signup {email}</Text>
+            <AccountDivider />
+            <AccountTextInput
+              mode="outlined"
+              label="Enter Email"
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+              left={<TextInput.Icon icon="mail" />}
+            />
+            <AccountTextInput
+              mode="outlined"
+              label="Enter Password"
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+              left={<TextInput.Icon icon="lock" />}
+            />
+            <AccountTextInput
+              mode="outlined"
+              label="Confirm Password"
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+              left={<TextInput.Icon icon="lock" />}
+            />
+          </AccountCard.Content>
+          <AccountCard.Actions>
+            <AccountButton>Cancel</AccountButton>
+          </AccountCard.Actions>
+        </AccountCard>
       </AccountBackground>
     </AccountScrollView>
   );
-};
-
-export default RegisterScreen;
+}
